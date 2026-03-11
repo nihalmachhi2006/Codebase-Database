6791db48a5ad64a4fa3ae31e900e0601d66583e4
Nihal Machhi <nihalmachhi11@gmail.com>
2026-03-11 09:02:38 +0530

    Start New Scripts

---
+++ DSA/Stack.py
@@ -0,0 +1,32 @@
+# LIFO - Last In First Out
+
+class Stack:
+    def __init__(self):
+        self.s = []
+    
+    def length(self):
+        return len(self.s)
+    
+    def push(self,value):
+        self.s.insert(0,value)
+
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s[0]
+        
+    def pop(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s.pop(0)
+        
+
+stk = Stack()
+stk.push(10)
+stk.push(20)
+stk.push(30)
+
+# print(stk.peek())
+print(stk.pop())
